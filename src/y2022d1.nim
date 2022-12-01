dfd7686f0ce6b81fe116d60ea0f900f577ace163
Callum Leslie <git@cleslie.uk>
2022-12-01 10:56:07 +0000

    Day 1 2022 shortened

--- src/y2022d1.nim
+++ src/y2022d1.nim
@@ -1,31 +1,13 @@
-import std/strutils
-import std/sequtils
+import std/[strutils,sequtils,algorithm]
 import aoc
 
 const
-    day = 1
-    year = 2022
+  day = 1
+  year = 2022
 
 var
-  input:seq[string] = getInput(day, year)
+  input:seq[string] = getInput(day, year, sep="\n\n")[0..^2]
   output:seq[int]
-  sum = 0
 
-for index, val in input:
-  if val == "":
-    output.add(sum)
-    sum = 0
-  else:
-    sum += parseInt(val)
-
-sum = 0
-
-echo "Task 1"
-echo $output.max()
-
-for i in 1..3:
-  sum += output.max()
-  output.delete(output.maxIndex())
-
-echo "Task 1"
-echo $sum
+output = input.mapIt(it.splitLines().mapIt(parseInt(it)).foldl(a+b)).sorted()[^3..^1]
+echo $output[^1] & " " & $output.foldl(a+b)
