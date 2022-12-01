5826a2791b5c742eeab07c6d3e4422233e15b7ef
Callum Leslie <git@cleslie.uk>
2022-12-01 10:55:43 +0000

    allow custom seperator

--- src/aoc.nim
+++ src/aoc.nim
@@ -1,9 +1,13 @@
 import std/httpclient
 import std/strutils
 
-proc getInput*(day: int, year: int) : seq =
+proc getInput*(day: int, year: int, sep: string = "") : seq =
   var client = newHttpClient()
   let cookieFile = open("cookie.txt")
   defer: cookieFile.close()
   client.headers = newHttpHeaders({ "cookie": cookie_file.readLine() })
-  return splitLines(client.getContent("https://adventofcode.com/" & $year & "/day/" & $day & "/input"))
+  var input = client.getContent("https://adventofcode.com/" & $year & "/day/" & $day & "/input")
+  if sep == "":
+    return splitLines(input)
+  else:
+    return split(input, sep)
