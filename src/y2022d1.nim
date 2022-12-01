8bf21c4a75a1aa60c3a710a31b183af074282fe7
Callum Leslie <git@cleslie.uk>
2022-12-01 05:13:48 +0000

    Day 1 2022

--- src/y2022d1.nim
+++ src/y2022d1.nim
@@ -1,4 +1,5 @@
 import std/strutils
+import std/sequtils
 import aoc
 
 const
@@ -7,3 +8,24 @@ const
 
 var
   input:seq[string] = getInput(day, year)
+  output:seq[int]
+  sum = 0
+
+for index, val in input:
+  if val == "":
+    output.add(sum)
+    sum = 0
+  else:
+    sum += parseInt(val)
+
+sum = 0
+
+echo "Task 1"
+echo $output.max()
+
+for i in 1..3:
+  sum += output.max()
+  output.delete(output.maxIndex())
+
+echo "Task 1"
+echo $sum
